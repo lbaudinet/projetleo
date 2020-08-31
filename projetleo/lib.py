ecac9ec98c5cc360b2c50a95b5c8c82dabf265ee
Leo Baudinet <leo_baudinet@hotmail.fr>
2020-08-31 12:50:01 +0200

    ok

--- projetleo/lib.py
+++ projetleo/lib.py
@@ -47,6 +47,9 @@ def clean_data(data):
     data.loc[:, 'Frequency'] = data['Frequency'].map(drows)
     return data
 
+def try_me():
+    print("Coucou petite perruche c'est moi l'élan derrière toi")
+
 
 if __name__ == '__main__':
     # For introspections purpose to quickly get this functions on ipython
